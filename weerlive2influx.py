f0deef2a9074a8e07997f5b8723dd63c84e4c4fb
Remco <76850907+RemBrandNL@users.noreply.github.com>
2021-02-27 10:25:48 +0100

    Update weerlive2influx.py

--- weerlive2influx.py
+++ weerlive2influx.py
@@ -11,13 +11,28 @@ import time
 import json
 from influxdb import InfluxDBClient
 from requests.exceptions import ConnectionError
+from os.path import expanduser, exists
+from os import getenv
+from configobj import ConfigObj
 
-InfluxHost = "x.x.x.x" # localhost or your ip 
-InfluxUser = "username"
-InfluxPass = "password"
-InfluxDB = "database2store"
-location = "Amsterdam" # any Dutch city in NL
-APIkey = "demo" # Get a free key on https://weerlive.nl/api/toegang/
+# Enter your credentials here to connect to influx
+InfluxHost = "127.0.0.1"      # IP or hostname, 127.0.0.1 if it runs local
+InfluxUser = "user"           # user-name that has access to the DB
+InfluxPass = "pass"           # password
+InfluxDB   = "database2store" # target db to store measurement
+
+influxConnectFile = expanduser("~/.influx.connect")
+
+if exists(influxConnectFile):
+	print("Found connection file (" + influxConnectFile + "), trying to update credentials...\n")
+	config = ConfigObj(influxConnectFile)
+	InfluxHost = config['INFLUX_HOST']
+	InfluxUser = config['INFLUX_USER']
+	InfluxPass = config['INFLUX_PASS']
+	InfluxDB   = config['INFLUX_DBAS']
+
+location = "Amsterdam" # any Dutch city
+APIkey   = "demo"      # Get a free key on https://weerlive.nl/api/toegang/
 
 url = "https://weerlive.nl/api/json-data-10min.php?key=" + APIkey + "&locatie=" + location
 response = requests.get(url)
@@ -68,11 +83,12 @@ weerliveData = [
 	"fields": weerDict
 	}
 	]
-print(json.dumps(weerliveData, indent=4))
+
+#print(json.dumps(weerliveData, indent=4))
 
 try:
 	dbClient = InfluxDBClient(InfluxHost, 8086, InfluxUser, InfluxPass, InfluxDB)
 	dbClient.write_points(weerliveData)
 	print("\n\nI saved Weerlive data!")
 except ConnectionError as e:
-	print("Can not connect to InfluxDB '" + InfluxDB + "' @" + InfluxHost + ", will try again next run.")
+	print("\n\nCan not connect to InfluxDB '" + InfluxDB + "' @" + InfluxHost + ", will try again next time.")
